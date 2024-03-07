9e009932110ad7dfd1b6102a2d6814d9175eef9d
Daniele <150018258+ddllv@users.noreply.github.com>
2024-03-07 12:13:12 +0100

    add input for multiple rounds

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,3 +1,4 @@
+// Computer's choice
 function genRandomNumber() {
     return Math.floor(Math.random() * 100);
 }
@@ -12,50 +13,61 @@ function getComputerChoice() {
     }
 }
 
-// console.log(getComputerChoice());
+// Results
+function playerWins(playerSelection, computerSelection) {
+    playerScore++;
+    return `Player wins!\nPlayer: ${playerSelection}, Computer: ${computerSelection}.\nScore: ${playerScore}.`;
+}
 
+function computerWins(playerSelection, computerSelection) {
+    computerScore++;
+    return `Computer wins!\nPlayer: ${playerSelection}, Computer: ${computerSelection}.\nScore: ${playerScore}.`;
+}
+
+function noneWins(playerSelection, computerSelection) {
+    return `Tie!\nPlayer: ${playerSelection}, Computer: ${computerSelection}.\nScore: ${playerScore}.`;
+}
+
+// RockPaperScissors Round calculation
 function playRockPaperScissors(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return `Tie! (Player: ${playerSelection}, Computer: ${computerSelection})`;
+    if (playerSelection === computerSelection) { // Tie.
+        return noneWins(playerSelection, computerSelection);
     }
 
-    if (
+    if ( // Player wins.
         (playerSelection === `paper` && computerSelection === `rock`) ||
         (playerSelection === `rock` && computerSelection === `scissors`) ||
         (playerSelection === `scissors` && computerSelection === `paper`)
     ) {
-        return `Player wins! (Player: ${playerSelection}, Computer: ${computerSelection})`;
-    } else {
-        return `Computer wins! (Player: ${playerSelection}, Computer: ${computerSelection})`;
+        return playerWins(playerSelection, computerSelection);
+    } else { // Computer wins.
+        return computerWins(playerSelection, computerSelection);
     }
-
-    // if (
-    //     (playerSelection === `paper` && computerSelection === `scissors`) ||
-    //     (playerSelection === `rock` && computerSelection === `paper`) ||
-    //     (playerSelection === `scissors` && computerSelection === `rock`)
-    // ) {
-    //     return `Computer wins! (Player: ${playerSelection}, Computer: ${computerSelection})`;
-    // }
 }
 
-/*
-Result: PLAYER
-paper vs rock = 1
-rock vs scissors = 1
-scissors vs paper = 1
+// RockPaperScissors Winner calculation
+function getWinner(playerScore, computerScore) {
+    if (playerScore === computerScore) {
+        return `Tie game! Player: ${playerScore}, Computer: ${computerScore}.`;
+    } else if (playerScore > computerScore) {
+        return `Player wins! Player: ${playerScore}, Computer: ${computerScore}.`;
+    } else {
+        return `Computer wins! Player: ${playerScore}, Computer: ${computerScore}.`;
+    }
+}
 
-Result: COMPUTER
-paper vs scissors = 2
-rock vs paper = 2
-scissors vs rock = 2
+// Game start
+function playGame() {
+    const computerSelection = getComputerChoice(); // Computer's choice.
+    const playerSelection = prompt(`What's your choice? Rock, Paper, or Scissors?`, `paper`).toLowerCase(); // Player's choice.
+    return playRockPaperScissors(playerSelection, computerSelection);
+}
 
-Result: TIE
-paper vs paper = tie
-rock vs rock = tie
-scissors vs scissors = tie
-*/
+let playerScore = 0;
+let computerScore = 0;
 
-const playerSelection = prompt(`What's your choice? Rock, Paper, or Scissors?`).toLowerCase(); // Player's choice.
-const computerSelection = getComputerChoice(); // Computer's choice.
+for (let rounds = prompt(`How many rounds do you wish to play?`, 5); rounds > 0; rounds--) {
+    console.log(playGame());
+}
 
-console.log(playRockPaperScissors(playerSelection, computerSelection));
+console.log(getWinner(playerScore, computerScore));
